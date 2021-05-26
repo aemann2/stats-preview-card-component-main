a72e478271724a9b9055506a5e77f5603c26d578
Adam Mann <aemann2@gmail.com>
2021-05-26 09:37:34 -0700

    styled mobile text

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      background: 'hsl(233, 47%, 7%)',
+      cardBackground: 'hsl(244, 38%, 16%)',
+      accent: 'hsl(277, 64%, 61%)',
+      textWhite: 'hsl(0, 0%, 100%)',
+      transWhite1: 'hsla(0, 0%, 100%, 0.75)',
+      transWhite2: 'hsla(0, 0%, 100%, 0.6)',
+    },
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        lexendDeca: ['"Lexend Deca"', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
